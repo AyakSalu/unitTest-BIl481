4cdc8dc2e23c659b3f8c994767e70b49de396e24
AyakSalu <aewyheq@gmail.com>
2024-03-28 21:38:53 +0300

    Update TestCompute.java

--- my-app/src/test/java/TestCompute.java
+++ my-app/src/test/java/TestCompute.java
@@ -14,4 +14,41 @@ public class TestCompute {
     c = new Compute(mq);
     assertTrue(true);
   }
+  
+  @Test
+  public void testSizeZero(){
+    MessageQueue mq = mock(MessageQueue.class);
+    c = new Compute(mq);
+    assertEquals(-1, c.countNumberOfOccurrences(""));
+    assertTrue(true);
+  }
+   
+  @Test
+  public void testContains(){
+    MessageQueue mq = mock(MessageQueue.class);
+    when(mq.size()).thenReturn(5);
+    when(mq.contains(anyString())).thenReturn(false);
+    c = new Compute(mq);
+    c.countNumberOfOccurrences("123");
+  }
+
+  @Test
+  public void testCounter(){
+    MessageQueue mq = mock(MessageQueue.class);
+    when(mq.size()).thenReturn(3);
+    when(mq.contains(anyString())).thenReturn(true);
+    //when(anyString().equals(anyString())).thenReturn(true);
+    c = new Compute(mq);
+    c.countNumberOfOccurrences("123");
+  }
+  @Test
+  public void testCounter2(){
+    MessageQueue mq = mock(MessageQueue.class);
+    when(mq.size()).thenReturn(3);
+    when(mq.contains(anyString())).thenReturn(true);
+    when(anyString().equals(mq.getAt(anyInt()))).thenReturn(false);
+    c = new Compute(mq);
+    c.countNumberOfOccurrences("123");
+  }
+ 
 }
